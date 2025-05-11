4c9f12ea3156dd32f12357d3189bb1a90c8c9e57
Motoki Hasegawa <mohasega@c2r4s4.42tokyo.jp>
2025-05-11 19:02:10 +0900

    0511

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mohasega <mohasega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 16:33:57 by mohasega          #+#    #+#             */
-/*   Updated: 2025/05/05 16:55:50 by mohasega         ###   ########.fr       */
+/*   Updated: 2025/05/11 18:53:18 by mohasega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	size_t	total_size;
 	void	*ptr;
 
+	if (nmemb != 0 && size > __SIZE_MAX__ / nmemb)
+		return (NULL);
 	total_size = nmemb * size;
 	ptr = malloc(total_size);
 	if (!ptr)
